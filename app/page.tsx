b746a197e757566ab84abd624a74b5f4ea585701
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-29 17:38:16 +0100

    j3r

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Faucet from '@/components/Faucet'
+import ConnectButton from '@/components/ConnectButton'
 
 export default function FaucetPage() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <ConnectButton />
       <Faucet />
     </main>
   )
