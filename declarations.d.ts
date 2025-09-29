098308514e0dfd2b40cd3d0cf194ed3fcd553c1f
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-29 17:09:11 +0100

    jwk

---
+++ declarations.d.ts
@@ -0,0 +1,9 @@
+// declarations.d.ts
+declare namespace JSX {
+  interface IntrinsicElements {
+    'w3m-button': React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    >
+  }
+}
